7348eaac554b5ee3cbc97e5a4d39b047230d815d
Xin Cong <82378681+xinconggg@users.noreply.github.com>
2024-05-29 11:23:32 +0800

    Coffee Machine (OOP) Project

--- Coffee Machine (OOP).py
+++ Coffee Machine (OOP).py
@@ -1,7 +1,7 @@
 class CoffeeMaker:
     # Models the machine that makes the coffee
     def __init__(self):
-        self.resources = {
+        self.resources = { 
             "water": 300,
             "milk": 200,
             "coffee": 100,
