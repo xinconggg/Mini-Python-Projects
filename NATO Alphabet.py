afa1b6d4c7400ab9be23497e17a7a62814c9c8b3
Xin Cong <82378681+xinconggg@users.noreply.github.com>
2024-06-07 14:36:31 +0800

    Update NATO Alphabet.py

--- NATO Alphabet.py
+++ NATO Alphabet.py
@@ -1,21 +1,4 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
 
 # Create a Dataframe then a Dictionary:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -25,4 +8,3 @@ phonetic_dict = {row.letter : row.code for (index,row) in data.iterrows()}
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
 print(output_list)
-
