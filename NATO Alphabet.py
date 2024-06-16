1aca242e48337a14c3da0f03a9f9429292841599
Xin Cong <82378681+xinconggg@users.noreply.github.com>
2024-06-16 20:32:32 +0800

    NATO Alphabet Project

--- NATO Alphabet.py
+++ NATO Alphabet.py
@@ -1,6 +1,6 @@
 import pandas
 
-# Create a Dataframe then a Dictionary:
+# Create a Dataframe then a Dictionary: 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter : row.code for (index,row) in data.iterrows()}
 
