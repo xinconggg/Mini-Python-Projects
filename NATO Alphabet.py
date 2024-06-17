9557ea9d4e4e8f34c89f5c6a49881e7913268f81
xinconggg <xincong123@gmail.com>
2024-06-17 20:10:33 +0800

    NATO Alphabet Project

--- NATO Alphabet.py
+++ NATO Alphabet.py
@@ -7,8 +7,4 @@ phonetic_dict = {row.letter : row.code for (index,row) in data.iterrows()}
 # Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
-<<<<<<< HEAD
 print(output_list)
-=======
-print(output_list)
->>>>>>> afa1b6d4c7400ab9be23497e17a7a62814c9c8b3
