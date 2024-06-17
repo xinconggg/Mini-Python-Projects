830f888c38f08963fb1d2c25dbb1b7300ed5d7a8
xinconggg <xincong123@gmail.com>
2024-06-17 20:11:43 +0800

    Automated Email Sender (Quotes) Project

--- Automated Email Sender (Quotes).py
+++ Automated Email Sender (Quotes).py
@@ -1,10 +1,18 @@
-from smtplib import *
+import datetime as dt
+import smtplib
+import random
 
 my_email = "my_email@gmail.com"
 my_password = "password"
-receipient_email = "receipient_email@gmail.com"
+now = dt.datetime.now()
+day_of_week_now = now.weekday()
 
-with SMTP("smtp.gmail.com") as connection:
-    connection.starttls() # To make the connection secure
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email, to_addrs=receipient_email, msg="Subject:Hello \n\nThis is to place the content of email.")
+# If today is Monday then randomly choose a quote from "quotes.txt"
+if day_of_week_now == 0: 
+    with open("quotes.txt") as quotes_file:
+        all_quotes = quotes_file.readlines()
+        quote = random.choice(all_quotes)
+    with smtplib.SMTP("smtp@gmail.com") as connection:
+        connection.starttls() # To ensure that the connection is secure
+        connection.login(my_email, my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject: Monday Motivational Quotes \n\n{quote}")
